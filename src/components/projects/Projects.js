b893752b616cfbaf43930a532a9bc8f796cf9fc1
hamidzzabdul <abdirahimabdul89@gmail.com>
2023-07-19 19:11:07 +0300

    added some animation

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,15 +1,41 @@
 import classes from "./Projects.module.css";
 import Project from "./Project";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
 
 const Projects = () => {
+  const [ref, inView] = useInView({
+    threshold: 0.2,
+    triggerOnce: true,
+  });
+  const animation = useAnimation();
+  useEffect(() => {
+    if (inView) {
+      animation.start({
+        y: 0,
+        opacity: 1,
+        transition: { ease: "easeIn", duration: 0.5 },
+      });
+    }
+
+    if (!inView) {
+      animation.start({
+        y: 55,
+        opacity: 0,
+        transition: { ease: "easeIn", duration: 0.5 },
+      });
+    }
+  }, [inView, animation]);
+
   return (
     <div className={classes.project}>
-      <div className={classes.title}>
-        <h1>
+      <motion.div animate={animation} className={classes.title}>
+        <h1 ref={ref}>
           <span>03.</span>Somethings i've built
         </h1>
         <div className={classes.line}></div>
-      </div>
+      </motion.div>
 
       <div className={classes["project-containers"]}>
         <Project />
